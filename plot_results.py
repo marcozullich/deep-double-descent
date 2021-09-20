a3adb8de5158f02e640c4edfeab67cb431fcbd5c
marcozullich <marco.zullich@gmail.com>
2021-09-20 11:42:15 +0200

    added image_file arg

--- plot_results.py
+++ plot_results.py
@@ -6,13 +6,14 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--file", type=str, required=True, help=".pt files with the results dict")
+    parser.add_argument("--origin_file", type=str, required=True, help=".pt files with the results dict")
+    parser.add_argument("--image_file", type=str, default="results.png", help="image file where the plot is going to be saved")
     args = parser.parse_args()
     # result_dict_1 = torch.load("results_cnn_kmnist.pt")
     # result_dict_2 = torch.load("results_cnn_kmnist2.pt")
     # result_dict = {**result_dict_1, **result_dict_2}
     # result_dict = result_dict_1
-    results_dict = torch.load(args.file)
+    results_dict = torch.load(args.origin_file)
     widths = sorted([k for k in result_dict.keys()])
     train = []
     test = []
@@ -24,4 +25,4 @@ if __name__ == "__main__":
     plt.plot(widths, np.log(train))
     plt.plot(widths, np.log(test))
     #plt.show()
-    plt.savefig("results.png")
+    plt.savefig(args.image_file)
